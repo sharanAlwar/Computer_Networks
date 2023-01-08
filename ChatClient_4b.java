9dc10401e4d200957952bba19ac227fb50b2cc20
sharanAlwar <93466740+sharanAlwar@users.noreply.github.com>
2023-01-08 12:16:59 +0530

    Create ChatClient_4b.java

---
+++ ChatClient_4b.java
@@ -0,0 +1,29 @@
+package cn_lab;
+
+import java.net.*;
+import java.io.*;
+public class chat_client_4b
+{
+    public static void main(String args[]) throws Exception
+    {
+        Socket sk=new Socket("127.0.0.1",2000);
+        BufferedReader sin=new BufferedReader(new InputStreamReader(sk.getInputStream()));
+        PrintStream sout=new PrintStream(sk.getOutputStream());
+        BufferedReader stdin=new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ( true )
+        {
+            System.out.print("Client : ");
+            s=stdin.readLine();
+            sout.println(s);
+            s=sin.readLine();
+            System.out.print("Server : "+s+"\n");
+            if ( s.equalsIgnoreCase("BYE") )
+                break;
+        }
+        sk.close();
+        sin.close();
+        sout.close();
+        stdin.close();
+    }
+}
